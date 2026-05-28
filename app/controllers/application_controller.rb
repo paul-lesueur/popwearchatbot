0934ecd8c95c970f8d7e322274a5c7e369eb6bfd
Anais Dumont anais.dumont6@gmail.com <anais.dumont6@gmail.com>
2026-05-28 17:18:05 +0200

    profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   layout :layout_by_resource
 
   private
+
+  def configure_permitted_parameters
+    # Champs autorisés à l'inscription
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :company_name, :phone])
+    # Champs autorisés à la modification du profil
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :company_name, :phone])
+  end
 
   def layout_by_resource
     if devise_controller? && devise_auth_pages?
@@ -14,7 +22,6 @@ class ApplicationController < ActionController::Base
   end
 
   def devise_auth_pages?
-    # Le bloc orange seulement pour connexion / inscription / mot de passe
     controller_name == "sessions" ||
       (controller_name == "registrations" && action_name == "new") ||
       controller_name == "passwords"
